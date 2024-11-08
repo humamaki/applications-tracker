ebcb2614daee0c6c879b37d39e78fe5789841de7
Ninja <DynamicNinja@proton.me>
2024-11-08 15:53:38 -0600

    Added Home Page

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+export default function About(){
+    return (
+        <div>
+            <h1>Welcome!</h1>
+            <p>
+                About US
+            </p>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function Home(){
+    return (
+        <div>
+            <Link href="/about">About Us</Link>
+            <h1>Welcome!</h1>
+            <p>
+                
+            </p>
+        </div>
+    )
+}
