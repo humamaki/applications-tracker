3a35dd4865fd4fb78b408a3262a8b2542bcdf0b9
Humam <humam.alfaiz@gmail.com>
2024-11-10 16:38:59 -0600

    more stuff

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir -p /home/ec2-user/nextjs-app
+cp -r . /home/ec2-user/nextjs-app
+cd /home/ec2-user/nextjs-app
 npm install next  --prefix /home/ec2-user/nextjs-app
 npm install --prefix /home/ec2-user/nextjs-app
 npm run build
